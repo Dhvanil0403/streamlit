3f65de74ab0fe7dd17b4e7e341c83eb5ad0ca95b
Dhvanil0403 <dhvanil0403@gmail.com>
2023-03-14 23:52:44 -0400

    Final

--- hello_world/streamlit_hello_world.py
+++ hello_world/streamlit_hello_world.py
@@ -3,36 +3,114 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.metrics import accuracy_score
 
-st.title('Uber pickups in NYC')
+st.title('Text Classification')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+# if st.checkbox('Show raw data'):
+#     st.subheader('Raw data')
+#     st.write(data)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+# st.subheader('Number of pickups by hour')
+# hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+# st.bar_chart(hist_values)
 
 # Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+#st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+#st.map(filtered_data)
+
+np.random.seed(500)
+
+# Add the Data using pandas
+Corpus = pd.read_csv(r"C:\Users\Dhvanil\Desktop\corpus.csv",encoding='latin-1')
+
+# Step - 1: Data Pre-processing - This will help in getting better results through the classification algorithms
+
+# Step - 1a : Remove blank rows if any.
+Corpus['text'].dropna(inplace=True)
+
+# Step - 1b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
+Corpus['text'] = [entry.lower() for entry in Corpus['text']]
+
+# Step - 1c : Tokenization : In this each entry in the corpus will be broken into set of words
+Corpus['text']= [word_tokenize(entry) for entry in Corpus['text']]
+
+# Step - 1d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
+
+# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+
+
+for index,entry in enumerate(Corpus['text']):
+    # Declaring Empty List to store the words that follow the rules for this step
+    Final_words = []
+    # Initializing WordNetLemmatizer()
+    word_Lemmatized = WordNetLemmatizer()
+    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    for word, tag in pos_tag(entry):
+        # Below condition is to check for Stop words and consider only alphabets
+        if word not in stopwords.words('english') and word.isalpha():
+            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
+            Final_words.append(word_Final)
+    # The final processed set of words for each iteration will be stored in 'text_final'
+    Corpus.loc[index,'text_final'] = str(Final_words)
+
+#print(Corpus['text_final'].head())
+
+# Step - 2: Split the model into Train and Test Data set
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['label'],test_size=0.3)
+
+# Step - 3: Label encode the target variable  - This is done to transform Categorical data of string type in the data set into numerical values
+Encoder = LabelEncoder()
+Train_Y = Encoder.fit_transform(Train_Y)
+Test_Y = Encoder.fit_transform(Test_Y)
+
+# Step - 4: Vectorize the words by using TF-IDF Vectorizer - This is done to find how important a word in document is in comaprison to the corpus
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(Corpus['text_final'])
+
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+
+# Step - 5: Now we can run different algorithms to classify out data check for accuracy
+
+# Classifier - Algorithm - Naive Bayes
+# fit the training dataset on the classifier
+Naive = naive_bayes.MultinomialNB()
+Naive.fit(Train_X_Tfidf,Train_Y)
+
+# predict the labels on validation dataset
+predictions_NB = Naive.predict(Test_X_Tfidf)
+
+
+
+# Use accuracy_score function to get the accuracy
+st.write("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, Test_Y)*100)
+
+
+# Classifier - Algorithm - SVM
+# fit the training dataset on the classifier
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(Train_X_Tfidf,Train_Y)
+
+# predict the labels on validation dataset
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+
+# Use accuracy_score function to get the accuracy
+st.write("SVM Accuracy Score -> ",accuracy_score(predictions_SVM, Test_Y)*100)
